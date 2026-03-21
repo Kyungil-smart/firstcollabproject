018e69d140b4205dbce75ea16f5736f514b50894
23M-RFT68 <kobh0221@gmail.com>
2026-03-21 15:46:49 +0900

    [fix] PlayerController Scriprt edit

--- Assets/WIP/KBH/PlayerController.cs
+++ Assets/WIP/KBH/PlayerController.cs
@@ -7,17 +7,24 @@ public class PlayerController : MonoBehaviour
     #region State
     [Header("Stats")] 
     [SerializeField] public float speed;
+    [SerializeField] public float health;
+    
     #endregion Stats
 
     #region Private
     private Rigidbody2D _rb;
     private Animator _anim;
-    private SpriteRenderer _sr;
+    private SpriteRenderer _sr;     // 캐릭터 좌우반전
     private bool _isAlive;
+    private float _currentHealth;
+    private float _maxHealth;
     #endregion
     
     #region Public
     public Vector2 inputVector { get; private set; }
+    public float MaxHealth  { get { return _maxHealth; } }
+    public float CurrentHealth { get { return _currentHealth; } }
+
     #endregion Public
     
     private void Awake()
@@ -33,10 +40,38 @@ public class PlayerController : MonoBehaviour
         _rb.MovePosition(_rb.position + nextVec);
     }
 
+    /*private void LateUpdate()
+    {
+        if (inputVector.x != 0)
+        {
+            _sr.flipX = inputVector.x < 0;
+        }
+    }*/
+
     private void OnMove(InputValue value)
     {
         inputVector = value.Get<Vector2>();
     }
 
+    public void TakeDamage(float damage)
+    {
+        _currentHealth -= damage;
+        if (_currentHealth <= 0)
+        {
+            // _anim.SetTrigger("Hurt");
+            _isAlive = false;
+            _currentHealth = 0;
+        }
+    }
 
+    public void Death()
+    {
+        if (!_isAlive)
+        {
+            // _anim.SetTrigger("Death");
+            GameOver();
+        }
+    }
+    
+    
 }
