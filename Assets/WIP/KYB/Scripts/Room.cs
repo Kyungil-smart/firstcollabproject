1364ceb73c3d9db4df7b9e38b579a9ada27c7de7
hiyougg11 <164721514+nekioEmilia@users.noreply.github.com>
2026-03-23 17:03:30 +0900

    [Feat] : TrySpawnRoom Method
    문이 생성되긴 하는데.. 버그가..

--- Assets/WIP/KYB/Scripts/Room.cs
+++ Assets/WIP/KYB/Scripts/Room.cs
@@ -1,13 +1,27 @@
 using UnityEngine;
 using TMPro;
 
+/// <summary>
+/// 테스트용 클래스
+/// </summary>
 public class Room : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI roomIndexText;
+    [Header("Door Prefabs")] [SerializeField]
+    private GameObject upDoor;
+
+    [SerializeField] private GameObject downDoor;
+    [SerializeField] private GameObject leftDoor;
+    [SerializeField] private GameObject rightDoor;
+
+    [Header("UI 컴포넌트 참조")] [SerializeField]
+    private TextMeshProUGUI roomIndexText;
 
     public int RoomIndex { get; private set; }
 
-
+    /// <summary>
+    /// 테스트용 메서드
+    /// </summary>
+    /// <param name="index"></param>
     public void SetRoomIndex(int index)
     {
         RoomIndex = index;
@@ -20,5 +34,13 @@ public class Room : MonoBehaviour
         {
             Debug.Log("Text 컴포넌트 연결되지 않았음");
         }
+    }
+
+    public void PlaceDoor(Vector2Int direction)
+    {
+        if (direction == Vector2Int.up) upDoor.SetActive(true); 
+        else if (direction == Vector2Int.down) downDoor.SetActive(true);
+        else if (direction == Vector2Int.left) leftDoor.SetActive(true);
+        else if (direction == Vector2Int.right) rightDoor.SetActive(true);
     }
 }
--- Assets/WIP/KYB/Scripts/Visualizer.cs
+++ Assets/WIP/KYB/Scripts/Visualizer.cs
@@ -4,31 +4,69 @@ using UnityEngine;
 public class Visualizer : MonoBehaviour
 {
     [SerializeField] private GameObject roomPrefab;
-    [SerializeField] private float roomOffset = 16f;
+    [SerializeField] private float roomOffset = 1f;
     
-    private List<GameObject> _spawnedRooms = new List<GameObject>();
+    private Dictionary<Vector2Int, Room> _roomDic =  new Dictionary<Vector2Int, Room>();
+    // private List<GameObject> _spawnedRooms = new List<GameObject>();
 
     public void VisualizeMap(HashSet<Vector2Int> floorPositions)
     {
         ClearMap();
-
+        int roomIndex = 0;
+        
         foreach (var position in floorPositions)
         { 
             Vector2 worldPosition = new Vector2(position.x *  roomOffset, position.y * roomOffset);
             
             GameObject newRoom = Instantiate(roomPrefab, worldPosition, Quaternion.identity);
-            _spawnedRooms.Add(newRoom);
-            newRoom.GetComponent<Room>().SetRoomIndex(_spawnedRooms.Count - 1);
+            Room room = newRoom.GetComponent<Room>();
+            room.SetRoomIndex(roomIndex++);
+            
+            _roomDic.Add(position,room);
+            
+            TrySpawnDoors();
+        }
+        
+        
+    }
+    
+    private void TrySpawnDoors()
+    {
+        foreach (var roomKey in _roomDic)
+        {
+            Vector2Int currentPosition = roomKey.Key;
+            Room currentRoom = roomKey.Value;
+
+            Vector2Int[] checkDirections = { Vector2Int.up, Vector2Int.right };
+
+            foreach (var direction in checkDirections)
+            {
+                Vector2Int targetPosition = currentPosition + direction;
+
+                if (_roomDic.TryGetValue(targetPosition, out Room neighborRoom))
+                {
+                    if (direction == Vector2Int.up)
+                    {
+                        currentRoom.PlaceDoor(Direction2D.CardinalDirectionList[2]);
+                        neighborRoom.PlaceDoor(Direction2D.CardinalDirectionList[0]);
+                    }
+                    else
+                    {
+                        currentRoom.PlaceDoor(Direction2D.CardinalDirectionList[3]);
+                        neighborRoom.PlaceDoor(Direction2D.CardinalDirectionList[1]);
+                    }
+                }
+            }
         }
     }
 
     private void ClearMap()
     {
-        foreach (var room in _spawnedRooms)
+        foreach (var room in _roomDic.Values)
         {
-            Destroy(room);
+            Destroy(room.gameObject);
         }
         
-        _spawnedRooms.Clear();
+        _roomDic.Clear();
     }
 }
