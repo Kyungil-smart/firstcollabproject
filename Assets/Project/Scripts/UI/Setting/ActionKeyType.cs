34b5455fd53a8a6c48cac37fdc8221f215b0b773
sjungah <sjungah99@gmail.com>
2026-03-25 16:03:56 +0900

    [Feat] 세팅 팝업 추가, 시작 화면 추가

---
+++ Assets/Project/Scripts/UI/Setting/ActionKeyType.cs
@@ -0,0 +1,15 @@
+namespace UI
+{
+    public enum ActionKeyType
+    {
+        MoveUp, 
+        MoveDown, 
+        MoveLeft, 
+        MoveRight, 
+        Interact, 
+        Weapon1, 
+        Weapon2,
+        Weapon3, 
+        Menu
+    }
+}
