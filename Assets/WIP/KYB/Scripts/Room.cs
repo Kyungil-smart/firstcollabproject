21745b661d05b6ad5dff6bddd1c1a2d060fe86e7
hiyougg11 <164721514+nekioEmilia@users.noreply.github.com>
2026-03-26 11:22:56 +0900

    [Feat] : SpawnPointRoutine method
    테스트 준비중입니다

--- Assets/WIP/KYB/Scripts/Room.cs
+++ Assets/WIP/KYB/Scripts/Room.cs
@@ -1,4 +1,8 @@
+using System;
+using Monster;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 
 public enum RoomType
@@ -8,11 +12,42 @@ public enum RoomType
     BossRoom
 }
 
-/// <summary>
-/// 테스트용 클래스
-/// </summary>
 public class Room : MonoBehaviour
-{
+{  
+    public Monster.MonsterSpawner spawner;
+    public List<Transform> spawnPoints;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnPointRoutine());
+    }
+
+    private IEnumerator SpawnPointRoutine()
+    {
+        while (true)
+        {
+            List<Vector2Int> offScreenPoint = new List<Vector2Int>();
+
+            foreach (var point in spawnPoints)
+            {
+                Vector3 viewPos = Camera.main.WorldToViewportPoint(point.position);
+
+                if (viewPos.x < 0 || viewPos.x > 1 || viewPos.y < 0 || viewPos.y > 1)
+                {
+                    Vector2Int girdPos = new Vector2Int(Mathf.RoundToInt(point.position.x), Mathf.RoundToInt(point.position.y));
+                    offScreenPoint.Add(girdPos);
+                }
+            }
+
+            if (spawner != null && spawnPoints.Count > 0)
+            {
+                spawner.UpdateSpawnableTiles(offScreenPoint);
+            }
+            
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
     [Header("Door Prefabs")] 
     [SerializeField] private GameObject upDoor;
     [SerializeField] private GameObject downDoor;
@@ -26,4 +61,6 @@ public class Room : MonoBehaviour
         else if (direction == Vector2Int.left) leftDoor.SetActive(true);
         else if (direction == Vector2Int.right) rightDoor.SetActive(true);
     }
+    
+    
 }
