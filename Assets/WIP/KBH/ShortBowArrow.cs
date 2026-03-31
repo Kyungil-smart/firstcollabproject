a2a597eb5667eab86e60aaadff555c8392049f6e
23M-RFT68 <kobh0221@gmail.com>
2026-03-31 16:32:15 +0900

    [feat] 숏보우 스크립트 생성

---
+++ Assets/WIP/KBH/ShortBowArrow.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+public class ShortBowArrow : MonoBehaviour
+{
+    public float speed = 20f;
+    public float lifeTime = 2f;
+    
+    private Vector3 _dir;
+
+    private void Start()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+        
+        _dir = (mousePos - transform.position).normalized;
+        
+        float angle = Mathf.Atan2(_dir.y, _dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void Update()
+    {
+        transform.position += _dir * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Monster") || collision.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
