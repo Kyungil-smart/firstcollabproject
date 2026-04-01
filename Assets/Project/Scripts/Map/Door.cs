781007588059692d5694b5caf904500cbbb24818
hiyougg11 <164721514+nekioEmilia@users.noreply.github.com>
2026-04-01 14:21:38 +0900

    [Fix] : targetRoomCount 10 -> 2

--- Assets/Project/Scripts/Map/Door.cs
+++ Assets/Project/Scripts/Map/Door.cs
@@ -7,12 +7,18 @@ public class Door : MonoBehaviour
     [Header("철창 프리팹")]
     [SerializeField] private GameObject ironBar;
 
+    /// <summary>
+    /// 방에 들어오면 철창 잠김
+    /// </summary>
     public void Close()
     {
         ironBar.SetActive(true);
         Debug.Log("문 닫김!");
     }
 
+    /// <summary>
+    /// 방 클리어하면 철창 열림
+    /// </summary>
     public void Open()
     {
         if (ironBar != null)
