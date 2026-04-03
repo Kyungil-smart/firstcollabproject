a6a70f5e97be60f41a67a62f04c23edcd3f40675
Masttte <peonyak08@gmail.com>
2026-04-03 12:27:08 +0900

    [fix] 몬스터 투사체 벽뚫수정

--- Assets/Project/Scripts/Monster/Objects/MonsterProjectile.cs
+++ Assets/Project/Scripts/Monster/Objects/MonsterProjectile.cs
@@ -2,66 +2,60 @@ using UnityEngine;
 
 namespace Monster
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class MonsterProjectile : MonoBehaviour
     {
         public float speed = 8f;
 
         static readonly int WallLayer = 9;
         static readonly int ObstacleLayer = 10;
-        
+
         private float _damage;
         private Vector2 _direction;
-        
-        // 거리 계산
-        private Vector2 _startPosition; 
+
+        private Vector2 _startPosition;
         private float _maxDistance;
-        
+
+        private Rigidbody2D _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _rb.bodyType = RigidbodyType2D.Kinematic;
+            _rb.gravityScale = 0f;
+        }
+
         public virtual void Init(Vector2 dir, float damage, float maxDistance)
         {
-            _direction = dir;
+            _direction = dir.normalized;
             _damage = damage;
             _maxDistance = maxDistance;
-            _startPosition = transform.position;
-            
-            // 날아가는 방향으로 회전
+            _startPosition = _rb.position;
+
             float angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+            _rb.linearVelocity = _direction * speed;
         }
-        
+
         private void Update()
         {
-            // 화살 날아가기
-            transform.position += (Vector3)(_direction * speed * Time.deltaTime);
-            
-            // 시작 위치로부터 날아간 거리가
-            // 최대 사거리에 도달하면 소멸
-            float traveledDistance = Vector2.Distance(_startPosition, transform.position);
-            if (traveledDistance >= _maxDistance)
+            if (Vector2.Distance(_startPosition, _rb.position) >= _maxDistance)
             {
-                Destroy(gameObject); 
+                Destroy(gameObject);
             }
         }
-        
+
         private void OnTriggerEnter2D(Collider2D other)
         {
-            // 플레이어 피격
             if (other.CompareTag("Player"))
             {
-                Debug.Log("Player hit " + other.name);
                 PlayerBody playerBody = MonsterManager.Instance.player.GetComponent<PlayerBody>();
-
-                if (playerBody != null)
-                {
-                    playerBody.TakeDamage(_damage);
-                }
-                else
-                {
-                    Debug.Log("PlayerBody Component is Null");
-                }
-                    
-                Destroy(gameObject); 
+                playerBody.TakeDamage(_damage);
+                Destroy(gameObject);
+                return;
             }
-            // 벽/장애물에 닿으면 소멸
+
             int layer = other.gameObject.layer;
             if (layer == WallLayer || layer == ObstacleLayer)
             {
